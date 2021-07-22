bf0a9c0e3fde5964041787e63eb687af8bcfd9c4
SASHANK DEB <69194538+Sashank-Deb@users.noreply.github.com>
2021-07-22 22:57:04 +0530

    Update and rename adminPannel.ts to admin.ts

---
+++ src/app/AdminPannel/admin.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin',
+  templateUrl: './admin.html',
+  styleUrls: ['./admin.css']
+})
+export class admin {
+  name = 'Angular ';i=0;
+}
