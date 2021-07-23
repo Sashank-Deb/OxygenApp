1eb3af494596f6a4471e05ab0bfd8c2303db255c
Ayonija <ayonija.mathur@gmail.com>
2021-07-23 21:53:27 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
-
+import { Component, OnInit, VERSION } from '@angular/core';
+import { v4 as uuid } from 'uuid';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit  {
   name = 'Angular ' + VERSION.major;
+  public list: string[] = [];
+  ngOnInit(){this.gen();}
+  public gen() {
+    console.log("UUID OF THE USER:"+uuid());
+    this.list.push(uuid());
+  }
 }
