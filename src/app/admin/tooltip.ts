7a71c2c9f6be9060253e569f3ce6bf72edef7284
SASHANK DEB <69194538+Sashank-Deb@users.noreply.github.com>
2021-07-24 06:27:45 +0530

    Update tooltip.ts

--- src/app/admin/tooltip.ts
+++ src/app/admin/tooltip.ts
@@ -3,7 +3,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 @Component({
   selector: 'admin-tooltip',
-  templateUrl: './tooltip.html'
+  templateUrl: './tooltip.html',
+  styleUrls: ['./tooltip.css']
 })
 export class TooltipDialog{  
   text_data=null;
